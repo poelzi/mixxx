bfd64251822e3d272ce7e38053f3c2af121fb1d7
Daniel Poelzleithner <git@poelzi.org>
2020-08-11 17:27:51 +0200

    Add BPM-Lock checkbox in track dialog
    Currently the user has to open the track in the library
    to enable/disable BPM lock. Now the BPM tab
    contains a bpmlock checkbox which allows the user to
    toggle the flag from the track dialog.

--- src/library/dlgtrackinfo.h
+++ src/library/dlgtrackinfo.h
@@ -51,6 +51,7 @@ class DlgTrackInfo : public QDialog, public Ui::DlgTrackInfo {
     void slotCancel();
 
     void trackUpdated();
+    void slotBpmLockChanged(int state);
 
     void slotBpmDouble();
     void slotBpmHalve();
@@ -86,6 +87,7 @@ class DlgTrackInfo : public QDialog, public Ui::DlgTrackInfo {
     void loadTrackInternal(const TrackPointer& pTrack);
     void populateFields(const Track& track);
     void reloadTrackBeats(const Track& track);
+    void enableBPMControls(bool enabled);
     void saveTrack();
     void unloadTrack(bool save);
     void clear();
