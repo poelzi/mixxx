4277954cef6dea60dfa6d93ab423af0341638aca
Daniel Poelzleithner <git@poelzi.org>
2020-08-26 22:55:51 +0200

    Add Rounding action to Context Menu
    Allows you to mass round from the library.

--- src/widget/wtrackmenu.h
+++ src/widget/wtrackmenu.h
@@ -96,6 +96,7 @@ class WTrackMenu : public QMenu {
     void slotLockBpm();
     void slotUnlockBpm();
     void slotScaleBpm(int);
+    void slotRoundBpm();
 
     // Info and metadata
     void slotShowDlgTagFetcher();
@@ -232,6 +233,7 @@ class WTrackMenu : public QMenu {
     QAction* m_pBpmThreeFourthsAction{};
     QAction* m_pBpmFourThirdsAction{};
     QAction* m_pBpmThreeHalvesAction{};
+    QAction* m_pBpmRoundAction{};
     QAction* m_pBpmResetAction{};
 
     // Track color
